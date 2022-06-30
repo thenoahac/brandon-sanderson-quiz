8a4182f27a56a09a441e2ed304588019a9b075f8
Noah Carralero <nole2kgames@gmail.com>
2022-06-30 00:52:11 -0400

    set start page in html and css, added java pseudo

---
+++ assets/script.js
@@ -0,0 +1,40 @@
+// questions array
+// currentQuestion = 0
+// timeLeft
+// score
+// highscores
+
+// startGame
+    // triggered by event listener on start game button
+    // start timer
+    // setup 1st question
+    // loadQuestion()
+
+// timer
+    // setInterval()
+    // render to page
+
+// loadQuestion 
+    // currentQeustion++
+    // render the question and multiple choices to the page
+    // btn1.textContent = 
+
+
+// checkAnswer 
+    // check which button user clicked
+    // compare the choice with correct answer
+    // act on right/wrong answer
+    // loadQuestion()
+
+// endGame 
+    // called at end of timer or when current question > # of questions
+    // cancel timer
+    // prompt for user initials
+    // local storage
+        // 1. read ls and store in js variable
+        // 2. check if null
+        // 3. update js variable with our new score
+        // 4. render to page
+        // 5. save updated js variable to ls
+    // make highscores visible
+    // prompt to play again?
